ca69b50393aac10f15ca42bbb1233e2365d02e3f
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-11-06 09:42:04 +0100

    formularz

--- inc/db.php
+++ inc/db.php
@@ -2,10 +2,11 @@
 function init_baza() {
 	global $db, $dbfile, $kom;
 	if (!file_exists($dbfile)) $kom[] = 'Brak pliku bazy. Tworzę nowy.';
-		
+
 	$db = new PDO("sqlite:$dbfile");
-	$db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+
 
 function init_tables() {
 	global $db;
@@ -13,22 +14,21 @@ function init_tables() {
 		$sql = file_get_contents(DBASE.'baza.sql', 'r');
 		$q = "SELECT name FROM sqlite_master WHERE type='table' AND name='menu'";
 		$ret = $db->query($q);
-		if (empty($ret)) {
-			$db->exec($sql);
-			$kom[] = "Utworzono tabele!";
-		}
+		//$db->exec($sql);
+		// $kom[] = "Utworzono tabele!";
 	}
 }
+
 
 function db_query($q, &$ret) {
 	global $db;
 	try {
 		$r = $db->query($q, PDO::FETCH_ASSOC);
-	}	catch(PDOException $e){
+	} catch(PDOException $e) {
 		echo ($e->getMessage());
 	}
 	if ($r) {
-		$ret = $r->fetchAll(); 
+		$ret = $r->fetchAll();
 		return true;
 	}
 	return false;
