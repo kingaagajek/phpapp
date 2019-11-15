1e788040308ca5348a04bfafcfc90518b3a53f3c
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-11-15 09:39:58 +0100

    użytkownik

--- db/baza.sql
+++ db/baza.sql
@@ -8,12 +8,23 @@ CREATE TABLE menu (
 
 INSERT INTO menu VALUES(NULL, 'witam', 'Witamy', 1);
 INSERT INTO menu VALUES(NULL, 'formularz', 'Formularz', 2);
-INSERT INTO menu VALUES(NULL, 'klasa', 'Klasa', 3);
+INSERT INTO menu VALUES(NULL, 'userform', 'Użytkownicy', 3);
+INSERT INTO menu VALUES(NULL, 'klasa', 'Klasa', 4);
 
 CREATE TABLE posty (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	tresc VARCHAR NOT NULL
 );
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	login CHAR(20) NOT NULL,
+	haslo CHAR(40),
+	email VARCHAR(50),
+	data DATE
+);
+
 
 
 -- INSERT INTO menu(tytul, plik, id) VALUES('Klasa', 'klasa', NULL);
