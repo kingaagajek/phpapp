261d845d169dba89b40fe99a2f8f738bc49f8ed5
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2020-02-19 09:40:55 +0100

    biblioteka jquery

---
+++ js/jquery_cwicz.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".wynik").text("Nowa treść");
+  console.log($("p").text()); // odczytanie tekstu z jakiegoś znacznika
+  $("#przycisk").click(function(){
+    ;
+  });
+	// wywołania jQuery
+});
