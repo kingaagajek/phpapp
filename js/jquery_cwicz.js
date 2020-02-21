ba3533032c3a3c4e70342267778782b32067eb46
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2020-02-21 10:37:02 +0100

    js, praca domowa

--- js/jquery_cwicz.js
+++ js/jquery_cwicz.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
   $(".wynik").text("Nowa treść");
-  console.log($("p").text()); // odczytanie tekstu z jakiegoś znacznika
+  console.log(($("p").text())); // odczytanie tekstu z jakiegoś znacznika
   $("#przycisk").click(function(){
-    ;
+    var vnk = parseInt($("#vnk").val());
+    var nk = parseInt($("#vnk").val());
+    var R = vnk / nk * 100;
+    var div2 = '<div class="wspolczynnik">'+R+'</div>';
+    $("div.wynik").after(div2);
   });
+  $("input").mouseover(function(){
+    $(this).addClass("tloBlue");
+  });
+  // podczas opuszczenia myszką obszaru input przywróć poprzedni kolor tła
 	// wywołania jQuery
 });
